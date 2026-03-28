9ba4472bb7f83e2b7374975c74673212d07259ba
eumyulchan <yulchaneum@gmail.com>
2026-03-28 12:28:11 +0000

    새 파일 추가: 1000.py 및 15552.py, 문제 정의 및 테스트 케이스 포함

---
+++ 1000.py
@@ -0,0 +1,15 @@
+ㅣ=[]
+e = 0
+for i in range(100):
+    l.append([0]*100)
+n = int(input())
+for i in range(n):
+    a,b = map(int, input().split())
+    for dd in range(b,b+10):
+         for i in range(a,a+10):
+             l[i][dd] = 1
+for i in range(100):
+    for dd in range(100):
+        if l[i][dd] == 1:
+            e +=1
+print(e)
