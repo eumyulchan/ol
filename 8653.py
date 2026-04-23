d198bd046dc907fbfd7288029b7b77b3c8adaa1b
eumyulchan <yulchaneum@gmail.com>
2026-04-23 11:52:48 +0000

    변수 이름 수정 및 이진 탐색 로직 개선 8653

--- 8653.py
+++ 8653.py
@@ -1,34 +1,43 @@
 import sys
 input = sys.stdin.readline
-ced, b = map(int, input().split())
+a, b = map(int, input().split())
 c = list(map(int, input().split()))
-
 cb = max(c)
 cbe = c.index(cb)
 def solve(d):
     s = 0
-    e = ced
+    e = a
+    ans = 0
+    wns = 0
     while True:
-         mid = (s+e)//2
-            
-         if mid == cbe:
-             print("T")
-             break
-         elif d == c[mid] and mid > cbe:
-            print("L")
-            break
-         elif d == c[mid] and mid < cbe:
-             print("R")
-             break
+            mid = (s+e)//2   
+            if mid == cbe and d == c[mid]:
+                print("T")
+                break
+            elif d == c[mid] and mid < cbe:
+                print("L")
+                break
+            elif d == c[mid] and mid > cbe:
+                print("R")
+                break
 
-         elif c[mid] > c.index(d):
-               e = mid - 1
-         elif c[mid] < c.index(d):
-               s = mid + 1
-         if s > e:
-             print("N")
-             break
-            
+            else:
+                e = mid -1
+                
+                if s == e:
+                   ans +=1
+           
+                if ans >= 2:
+                    s = (s+a)//2
+                    
+                    e = a
+                    wns +=1
+                if wns >= 2:
+                  break   
+                elif s > e:
+                    print("N")
+                    break
+           
 for i in range(b):
     se = int(input())
     solve(se)
