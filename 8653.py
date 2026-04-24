eaa9349ee0888e2d90f2547a5b1080fdc0de1605
eumyulchan <yulchaneum@gmail.com>
2026-04-24 10:06:16 +0000

    입력 형식 수정 및 이진 탐색 로직 개선

--- 8653.py
+++ 8653.py
@@ -1,45 +1,39 @@
 import sys
 input = sys.stdin.readline
-a, b = map(int, input().split())
-c = list(map(int, input().split()))
-cb = max(c)
-cbe = c.index(cb)
-def solve(d):
-    s = 0
-    e = a
-    ans = 0
-    wns = 0
-    while True:
-            mid = (s+e)//2   
-            if mid == cbe and d == c[mid]:
-                print("T")
-                break
-            elif d == c[mid] and mid < cbe:
-                print("L")
-                break
-            elif d == c[mid] and mid > cbe:
-                print("R")
-                break
+n,q = map(int, input().split())
+l = list(map(int, input().split()))
+ql = list(map(int, input().split()))
+o = 0
+c = []
+c = set(l)
 
-            else:
-                e = mid -1
-                
-                if s == e:
-                   ans +=1
+def solve(ab):
+    s = 0
+    e = n-1
+    ol = 0
+   
+    while True:
+
+        mid = (s+e)//2
+        
+        if l[mid] == ab:
+        
+            break
+        elif l[mid] > ab:
+            e = mid - 1
+         
+        elif l[mid] < ab:
+            s = mid+1
+        if s>e:
+            ol += 1
+            print(ab, end = ' ')
+            
+            return ol
            
-                if ans >= 2:
-                    s = (s+a)//2
-                    
-                    e = a
-                    wns +=1
-                if wns >= 2:
-                    print("N")
-                    break   
-                elif s > e:
-                    print("N")
-                    break
-           
-for i in range(b):
-    se = int(input())
-    solve(se)
-    se = 0
+
+for i in ql:
+    solve(i)
+ol = ol + solve(i)
+
+if  ol == 0:
+    print(-1)
