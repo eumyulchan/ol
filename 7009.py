e925c6195c62b46ae07867f00f0c7334020ca71d
eumyulchan <yulchaneum@gmail.com>
2026-04-24 10:45:20 +0000

    이진 탐색 로직 개선 및 불필요한 변수 제거

--- 7009.py
+++ 7009.py
@@ -5,35 +5,34 @@ l = list(map(int, input().split()))
 ql = list(map(int, input().split()))
 o = 0
 c = []
-c = set(l)
-
+l.sort()
+ans = 0
+ans1 = 0
 def solve(ab):
     s = 0
     e = n-1
-    ol = 0
+   
    
     while True:
 
         mid = (s+e)//2
         
         if l[mid] == ab:
-        
-            break
+            
+            return 1
         elif l[mid] > ab:
             e = mid - 1
-         
         elif l[mid] < ab:
             s = mid+1
         if s>e:
-            ol += 1
-            print(ab, end = ' ')
-            
-            return ol
+            return 0
            
 
 for i in ql:
     solve(i)
-ol = ol + solve(i)
-
-if  ol == 0:
+    ans = solve(i)
+    ans1 = ans1 + ans
+    if ans == 0:
+        print(i, end = ' ')
+if  ans1 == q:
     print(-1)
